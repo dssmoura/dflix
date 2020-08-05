f291dcecc2197d634bc9f534f0d7e460dcf37c05
danilo.moura <danilo.moura@luizalabs.com>
2020-08-05 08:52:45 -0300

    Aula 2

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo (){
+  return(
+    <div>
+      Página de cadastro de videos
+    </div>
+  )
+}
+
+const Pagina404 = () =>(<div>Pagina 404</div>)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={App} exact />
+      <Route component={Pagina404} exact />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
